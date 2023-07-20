5b3cdb67778d6c1b972943d14f5f5f848093e12b
Julio Jimenez <julius.1986@gmail.com>
2023-07-20 16:58:59 +0100

    Gifs App - Fin Seccion 7

---
+++ src/app/gifs/pages/home/home-page.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+import { Gif } from '../../interfaces/gifs.interfaces';
+
+@Component({
+  selector: 'gifs-home-page',
+  templateUrl: './home-page.component.html'
+})
+export class HomePageComponent {
+
+
+
+    constructor( private gifService: GifsService ) {}
+
+    get gifs(): Gif[] {
+      return this.gifService.gifList;
+    }
+}
